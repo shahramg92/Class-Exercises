5da8dae45040a30a2f57e631270afa642b0c0522
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-06 10:21:06 -0500

    update

--- Week-1/Python-Exercises-3/function6.py
+++ Week-1/Python-Exercises-3/function6.py
@@ -1,14 +1,8 @@
-import matplotlib.pyplot as plot
-import math
-from numpy import arange
 
-def f(x):
-    return math.sin(x)
-
-xs = arange(-5, 5, 0.1)
-ys = []
-for x in xs:
-    ys.append(f(x))
-
-plot.plot(xs, ys)
-plot.show()
+def f(n):
+    if n < 0:
+        return n * 2
+    elif n % 2 == 0:
+        return "hey"
+    else:
+        return False
