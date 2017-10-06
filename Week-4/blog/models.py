7526b9a50dcb2d74517ab409594969a2747def2e
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-06 10:26:16 -0500

    update

--- Week-4/blog/models.py
+++ Week-4/blog/models.py
@@ -3,12 +3,21 @@ import os
 
 import peewee
 from playhouse.db_url import connect
+
 DB = connect(
   os.environ.get(
     'DATABASE_URL',
-    'postgres://postgres:postgres@localhost:5432/mydb'
+    'postgres://localhost:5432/blog' #532 is default port for postgres
   )
 )
+
 class BaseModel (peewee.Model):
   class Meta:
     database = DB
+
+class BlogPost (BaseModel):
+  title = peewee.CharField(max_length=60)
+  slug = peewee.CharField(max_length=50, unique=True)
+  body = peewee.TextField()
+  created = peewee.DateTimeField(
+              default=datetime.datetime.utcnow)
