7474f75598e75088d626eaec29ecab78a12f4191
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-12 09:58:13 -0500

    update

--- Week-5/JavaScript 1/tipcalc.js
+++ Week-5/JavaScript 1/tipcalc.js
@@ -1,6 +1,10 @@
-function tipAmount (good, fair, bad) {
-  var
+function tipAmount (bill, good, fair, bad) {
+  var bill = prompt(' What is the total bill? ')
+  var 
+  var tip =
 
-
+  var good = 0.2;
+  var fair = 0.15;
+  var bad = 0.10;
 
 }
