50ada8f5d77422da7f02d2961cc8871fbbc4d3ef
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-13 14:03:07 -0500

    update

--- Week-5/JavaScript 1/madlib.js
+++ Week-5/JavaScript 1/madlib.js
@@ -1,3 +1,7 @@
+// Write a madlib function, which is given a name and a subject. It will return(not print) a new string: (name)'s favorite subject in school is (subject).
+
+
+
 function madlib (name, subject) {
   var output = name + ' favorite subject is ' + subject;
   console.log(output);
