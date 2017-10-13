c1001490086f96966e85f49ebfb58d7fff4a8f34
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-13 14:03:50 -0500

    update

--- Week-5/JavaScript 1/tipcalc.js
+++ Week-5/JavaScript 1/tipcalc.js
@@ -1,8 +1,15 @@
+// Write a function tipAmount that is given the bill amount and the level of service (one of good, fair and poor) and returns the dollar amount for the tip. Based on:
+//
+// good -> 20%
+// fair -> 15%
+// bad -> 10%
+//
+
+
 function tipAmount (bill, good, fair, bad) {
   var bill = prompt(' What is the total bill? ')
-  var 
+  var
   var tip =
-
   var good = 0.2;
   var fair = 0.15;
   var bad = 0.10;
