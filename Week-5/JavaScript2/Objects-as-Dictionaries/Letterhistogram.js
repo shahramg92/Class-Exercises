4f9c1fa71f657f7ce89deafb8393edf88bee947a
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-16 11:24:32 -0500

    update

---
+++ Week-5/JavaScript2/Objects-as-Dictionaries/Letterhistogram.js
@@ -0,0 +1,20 @@
+// Write a function letterHistogram which takes a string as argument. It will tally(histogram) the number of times each character appears in the string, and return the tally as an object. Example:
+// > letterHistogram('bananas')
+// { b: 1, a: 3, n: 2, s: 1 }
+// Note: the order of the keys doesn't matter.
+
+function letterHistogram(string) {
+  let histogram = {};
+    for (i = 0; i < string.length; i++) {
+      let char = string[i]
+      if (char in histogram) {
+        histogram[char] += 1;
+      }
+      else {
+        histogram[char] = 1;
+      }
+    }
+    return histogram;
+  }
+
+console.log(letterHistogram('bananas'));
---
+++ Week-5/JavaScript2/Objects-as-Dictionaries/Wordhistogram.js
@@ -0,0 +1,7 @@
+function wordHistogram(string)  {
+
+
+
+
+  
+}
