89ecedef94044b55f5fba10b88fb68249ba72059
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-15 22:03:57 -0500

    update

---
+++ Week-5/JavaScript2/Objects-as-Dictionaries/Exercise1.js
@@ -0,0 +1,8 @@
+// Given the following dictionary, representing a mapping from names to phone numbers:
+// Write code to do the following:
+//
+// Print Elizabeth's phone number.
+// Add a entry to the dictionary: Kareem's number is 938-489-1234.
+// Delete Alice's phone entry.
+// Change Bob's phone number to '968-345-2345'.
+// Given this code var personName = 'Elizabeth';, use the variable personName to access the dictionary entry. Use a for...in loop to print all the phone entries.
