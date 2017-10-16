7f179a4d274f034a421b9cd7ab1c3e94a3297bd2
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-15 19:41:57 -0500

    update

--- Week-5/JavaScript 1/longvowels.js
+++ Week-5/JavaScript 1/longvowels.js
@@ -1,1 +1,18 @@
-Write a function, which is given a string, return the result of extending any long vowels to the length of 5.
+// Write a function, which is given a string, return the result of extending any long vowels to the length of 5.
+
+function longLongVowels(string) {
+  let vowels = ['a', 'e', 'i', 'o', 'u']
+  let output = []
+  for (i = 0; i < string.length; i++) {
+    let char = string[i]
+    if (vowels.indexOf(char.toLowerCase()) != -1) {
+      if (string[i].toLowerCase() === string[i + 1].toLowerCase()) {
+        output.push(char.repeat(3));
+      }
+    }
+    output.push(char);
+  }
+  return output.join('')
+}
+
+console.log(longLongVowels('I love using long vowels!!'))
