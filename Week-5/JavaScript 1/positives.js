e90b8cb7150550fc86432a5c97407aafdc58747b
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-15 19:53:27 -0500

    update

--- Week-5/JavaScript 1/positives.js
+++ Week-5/JavaScript 1/positives.js
@@ -1,1 +1,14 @@
 // Write a function positiveNumbers which is given an array of numbers and returns a new array containing only the positive numbers within the given array.
+
+function positiveNumbers(input) {
+  positivelist = []
+  for (i = 0; i < input.length; i++) {
+    if (input[i] > 0) {
+      positivelist.push(input[i])
+    }
+  }
+  return positivelist
+}
+
+array = [-1, -2, -3, 5, 3, -5]
+console.log(positiveNumbers(array))
