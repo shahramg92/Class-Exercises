f6c0e568ec1f2fec058b5b276ac84c2aff6f0b74
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-15 19:55:24 -0500

    update

--- Week-5/JavaScript 1/caesarcipher.js
+++ Week-5/JavaScript 1/caesarcipher.js
@@ -1,1 +1,24 @@
 // Write a function cipher which is given a string, an offset, and returns the Caesar cipher of the string.
+
+function caesar(string, offset) {
+  let cipher = []
+  for (i = 0; i < string.length; i++ ) {
+    char = string[i];
+    let charcode = char.charCodeAt(0)
+    if (charcode >= 65 && charcode < 91 || charcode >= 97 && charcode < 123) {
+      let newcharcode = (charcode + offset)
+      if (newcharcode > 122 || (newcharcode >= 91 && newcharcode < 97) ) {
+        newcharcode = newcharcode - 26;
+      }
+      var newchar = String.fromCharCode(newcharcode)
+    }
+    else {
+      var newchar = char
+    }
+    cipher.push(newchar)
+  }
+
+  return cipher.join('')
+}
+
+console.log(caesar('abcdefghijklmnopqrstuvwxyz./?,<>;{}][\';\']\''.toUpperCase(), 1))
--- Week-5/JavaScript 1/caesarcipher2.js
+++
@@ -1,1 +0,0 @@
-// Write a function decipher which is given a string, an offset, and returns the original message.
