c4ee23423d539bc10f760f702e1b78891460c6ea
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-16 14:38:15 -0500

    update

--- Week-5/JavaScript2/Functions/sumararray.js
+++ Week-5/JavaScript2/Functions/sumararray.js
@@ -1,1 +1,8 @@
 // Write a function sum that takes an array of numbers as argument and returns the sum of those numbers. Use the reduce method to do this.
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(array.reduce(function getSum (x, y) {
+  console.log(x, y);
+  return x + y;
+}));
