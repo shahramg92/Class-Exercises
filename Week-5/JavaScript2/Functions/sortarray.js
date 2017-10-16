a9451a660322289469ded840d6f6d9d54a6b39de
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-16 13:35:59 -0500

    update

---
+++ Week-5/JavaScript2/Functions/sortarray.js
@@ -0,0 +1,13 @@
+// Given an array of strings such the array of names given in the previous problem, sort them by alphabetically order.
+
+var people = [
+  'Dom',
+  'Lyn',
+  'Kirk',
+  'Autumn',
+  'Trista',
+  'Jesslyn',
+  'Kevin',
+  'John',
+  'Eli',
+];
