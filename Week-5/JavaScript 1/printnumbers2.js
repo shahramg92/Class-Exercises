c8c749ad900af452c87c1a5f505a4b52c464a00f
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-15 19:06:20 -0500

    update

--- Week-5/JavaScript 1/printnumbers2.js
+++ Week-5/JavaScript 1/printnumbers2.js
@@ -1,4 +1,8 @@
 // Write a function printNumbers which is given a start number and an end number. It will print the numbers from one to the other, one per line:
 // Write two versions of the above function. One using a while loop and the other using a for loop.
 
-function 
+var i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
