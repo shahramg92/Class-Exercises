038d67a71b6a6f5b4a31988a1b32c4c5213dc708
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-16 13:38:53 -0500

    update

---
+++ Week-5/JavaScript2/Functions/sortarray2.js
@@ -0,0 +1,16 @@
+// Sort the same array, but not by alphabetically order, but by how long each name is, shortest name first.
+//
+
+var people = [
+  'Dom',
+  'Lyn',
+  'Kirk',
+  'Autumn',
+  'Trista',
+  'Jesslyn',
+  'Kevin',
+  'John',
+  'Eli',
+];
+
+console.log(people.sort());
