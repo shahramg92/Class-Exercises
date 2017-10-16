3f485d88bfd17e59e3573d1cdad3b0dc51f21a4d
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-15 22:15:00 -0500

    update

--- Week-5/JavaScript2/Objects-as-Dictionaries/Exercise1.js
+++ Week-5/JavaScript2/Objects-as-Dictionaries/Exercise1.js
@@ -6,3 +6,14 @@
 // Delete Alice's phone entry.
 // Change Bob's phone number to '968-345-2345'.
 // Given this code var personName = 'Elizabeth';, use the variable personName to access the dictionary entry. Use a for...in loop to print all the phone entries.
+
+var phonebookDict = {
+  Alice: '703-493-1834',
+  Bob: '857-384-1234',
+  Elizabeth: '484-584-2923'
+}
+
+console.log(phonebookDict.Elizabeth);
+phonebookDict.Kareem = '938-489-1234';
+delete phonebookDict.Alice;
+phonebookDict.Bob = '968-345-2345';
