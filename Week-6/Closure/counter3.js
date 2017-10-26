c3f0dc2ed6358a39e26e53dee668f097ec748205
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-26 12:51:58 -0500

    update

---
+++ Week-6/Closure/counter3.js
@@ -0,0 +1,1 @@
+// Allow the counter to either increment 1 or decrement by 1 by calling the increment and decrement methods, respectively. Note: now the counter needs to return an object rather than a function.
