f1e9e08049ded9bde08732b9b5a8c2f5a5651aef
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-26 12:47:22 -0500

    update

--- Week-6/Closure/counter2.js
+++ Week-6/Closure/counter2.js
@@ -1,1 +1,14 @@
 // Allow the caller of counter to initialize the count to the first argument that's passed in.
+
+function counter (start) {
+  var actuallyCount = function () {
+    start++;
+    return start;
+  }
+  return actuallyCount;
+}
+
+var x = counter(3);
+console.log(x());
+console.log(x());
+console.log(x());
