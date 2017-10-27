5dac6fb5b3c97a3278e283c556c3e4d677bafa4e
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-27 15:20:42 -0500

    update

--- Week-6/More-JavaScript-Objects/person.js
+++ Week-6/More-JavaScript-Objects/person.js
@@ -1,28 +1,28 @@
-// Given this Person type:
-
-function Person(name) {
-  this.name = name;
-  this.friends = [];
-}
-
-Person.prototype.addFriend = function(friend) {
-  this.friends.push(friend);
-};
-
-Person.prototype.createGreeting = function(other) {
-  return 'Yo ' + other.name + '! from ' + this.name + '.';
-};
-
-Person.prototype.greet = function(other) {
-  console.log(this.createGreeting(other));
-};
-
-var p1 = new Person('Me');
-var p2 = new Person('You');
-
-p1.createGreeting(p2);
-
-Rewrite the above type as a class
+// // Given this Person type:
+//
+// function Person(name) {
+//   this.name = name;
+//   this.friends = [];
+// }
+//
+// Person.prototype.addFriend = function(friend) {
+//   this.friends.push(friend);
+// };
+//
+// Person.prototype.createGreeting = function(other) {
+//   return 'Yo ' + other.name + '! from ' + this.name + '.';
+// };
+//
+// Person.prototype.greet = function(other) {
+//   console.log(this.createGreeting(other));
+// };
+//
+// var p1 = new Person('Me');
+// var p2 = new Person('You');
+//
+// p1.createGreeting(p2);
+//
+// Rewrite the above type as a class
 
 class Person {
   constructor(name) {
@@ -30,7 +30,7 @@ class Person {
     this.friends = [];
   }
 
-  addFriend(other) {
+  addFriend(friend) {
     this.friends.push(friend);
   }
 
@@ -39,24 +39,7 @@ class Person {
   }
 
   greet(other) {
-    
+    console.log(this.createGreeting(other));
   }
 
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-kf
