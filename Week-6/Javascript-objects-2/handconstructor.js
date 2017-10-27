c8f1995547d470db6ec3c9dd8b0efa981ddc4a9d
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-27 13:14:34 -0500

    update

---
+++ Week-6/Javascript-objects-2/handconstructor.js
@@ -0,0 +1,10 @@
+// Hand constructor
+//
+// A hand is simply represented as a collection of cards, but it would also be convenient for a hand to be able to return it's point value. We would like to be able to do this with a Hand constructor:
+//
+// > var myHand = new Hand()
+// > myHand.addCard(new Card(5, 'diamonds'))
+// > myHand.addCard(new Card(13, 'spades'))
+// > myHand.getPoints()
+// 15
+// Implement a Hand constructor that will allow the code above to work. Hint: you will want to store as a property of a hand object - an array of card objects.
