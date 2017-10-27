44214dc8e506086140b8090a8ddfe14c2645419e
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-27 15:04:44 -0500

    update

--- Week-6/More-JavaScript-Objects/person.js
+++ Week-6/More-JavaScript-Objects/person.js
@@ -1,18 +1,48 @@
 // Given this Person type:
-// function Person(name) {
-//   this.name = name;
-//   this.friends = [];
-// }
-//
-// Person.prototype.addFriend = function(friend) {
-//   this.friends.push(friend);
-// };
-//
-// Person.prototype.createGreeting = function(other) {
-//   return 'Yo ' + other.name + '! from ' + this.name + '.';
-// };
-//
-// Person.prototype.greet = function(other) {
-//   console.log(this.createGreeting(other));
-// };
-// Rewrite the above type as a class
+
+function Person(name) {
+  this.name = name;
+  this.friends = [];
+}
+
+Person.prototype.addFriend = function(friend) {
+  this.friends.push(friend);
+};
+
+Person.prototype.createGreeting = function(other) {
+  return 'Yo ' + other.name + '! from ' + this.name + '.';
+};
+
+Person.prototype.greet = function(other) {
+  console.log(this.createGreeting(other));
+};
+
+var p1 = new Person('Me');
+var p2 = new Person('You');
+
+p1.createGreeting(p2);
+
+Rewrite the above type as a class
+
+class Person {
+  constructor(name, friends) {
+    this.name = name;
+    this.friends = friends;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+kf
