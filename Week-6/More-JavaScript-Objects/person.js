ff025f6f930437530f316027ee2aa01fd0e1aa4d
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-27 14:35:48 -0500

    update

--- Week-6/More-JavaScript-Objects/person.js
+++ Week-6/More-JavaScript-Objects/person.js
@@ -1,10 +1,18 @@
-var paul = {
-  fname: 'Paul',
-  lname: 'Bailey',
-  greet: function () {
-    console.log(`Hello ${this.fname} ${this.lname}`);
-  }
-};
-var shannon = {fname: 'Shannon'};
-shannon.__proto__ = paul;
-shannon.greet();
+// Given this Person type:
+// function Person(name) {
+//   this.name = name;
+//   this.friends = [];
+// }
+//
+// Person.prototype.addFriend = function(friend) {
+//   this.friends.push(friend);
+// };
+//
+// Person.prototype.createGreeting = function(other) {
+//   return 'Yo ' + other.name + '! from ' + this.name + '.';
+// };
+//
+// Person.prototype.greet = function(other) {
+//   console.log(this.createGreeting(other));
+// };
+// Rewrite the above type as a class
