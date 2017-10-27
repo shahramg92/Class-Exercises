1687dd9495fb74cc414cea9415475c6dff0e8bc6
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-27 13:42:56 -0500

    update

---
+++ Week-6/More-JavaScript-Objects/inheritance.js
@@ -0,0 +1,9 @@
+// Given the following objects:
+//
+// One
+//
+// Write code to make daughter inherit properties from mom. What are the daugther's properties and their values? Print them out.
+//
+// Two
+//
+// Add a method called showInfo to mom by attaching a function to it as a property. Calling this method will print out all four properties. Call this method on both mom and daugther.
