eb5092b7eb482c3f227c263353738193bad6ae64
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-27 15:46:47 -0500

    update

--- Week-6/More-JavaScript-Objects/thesethises.js
+++ Week-6/More-JavaScript-Objects/thesethises.js
@@ -1,1 +1,29 @@
 // Given the Person type given in the last problem, add to it a lazyGreet method which will print out the same greeting message but only after 2 seconds.
+
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.friends = [];
+  }
+
+  addFriend(friend) {
+    this.friends.push(friend);
+  }
+
+  createGreeting(other) {
+    return 'Yo ' + other.name + '! from ' + this.name + '.';
+  }
+
+  greet(other) {
+    console.log(this.createGreeting(other));
+  }
+
+  setTimeout(lazyGreet(lazy)){
+    console.log('Yo ' + $(lazy.name) + '! from ' + $(this.name) + '.');
+  }
+}
+
+var p1 = new Person('Shahram');
+var p2 = new Person('Sofia');
+
+console.log(p1.lazyGreet(p2));
