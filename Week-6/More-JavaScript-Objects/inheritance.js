a8cd3b4ea81e21ab846277c326d4a8bd82b8f157
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-27 13:44:01 -0500

    update

--- Week-6/More-JavaScript-Objects/inheritance.js
+++ Week-6/More-JavaScript-Objects/inheritance.js
@@ -7,3 +7,14 @@
 // Two
 //
 // Add a method called showInfo to mom by attaching a function to it as a property. Calling this method will print out all four properties. Call this method on both mom and daugther.
+
+var mom = {
+  firstName: 'Alice',
+  lastName: 'Wong',
+  eyeColor: 'brown',
+  hairColor: 'black'
+};
+var daughter = {
+  firstName: 'Ilene',
+  hairColor: 'brown'
+};
