4f70f2c9b8709f7c654183413d19f2b820260c29
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-30 11:22:16 -0500

    update

---
+++ Week-7/Callback/funcionrw.js
@@ -0,0 +1,20 @@
+// Rewrite the following normal functions into callback functions.
+//
+// function add(x, y) {
+//   var result = x + y;
+//   return result;
+// }
+//
+// function subtract(x, y) {
+//   return x - y;
+// }
+//
+// function greeting(person) {
+//   return 'Hola, ' + person + '!';
+// }
+//
+// function product(numbers) {
+//   return numbers.reduce(function(a, b) {
+//     return a * b;
+//   }, 1);
+// }
