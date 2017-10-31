60b7b3f8471c8e966369e446e80b4e135a616b83
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-31 15:31:24 -0500

    week 7

---
+++ Week-7/npm-exercises/bonusnode2.js
@@ -0,0 +1,3 @@
+// Bonus 2: Any module
+//
+// Find any npm module and learn how to use it by reading its documentation. You can do a google search for something you might be interested in and add "npm" to the search term. Or you can look through this list for some inspiration.
