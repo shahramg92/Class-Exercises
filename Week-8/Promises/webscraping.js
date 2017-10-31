cd23c4a9165137198e29b1a3b29ea05af8b211bb
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-31 15:37:42 -0500

    week 7

--- Week-8/Promises/webscraping.js
+++ Week-8/Promises/webscraping.js
@@ -8,3 +8,15 @@
 //   'https://en.wikipedia.org/wiki/Google_Chrome'
 // ];
 // Use Promise.all and request-promise to retrieve the HTML files for all the web pages.
+
+var rp = require('request-promise');
+
+
+
+var urls = [
+  'https://en.wikipedia.org/wiki/Futures_and_promises',
+  'https://en.wikipedia.org/wiki/Continuation-passing_style',
+  'https://en.wikipedia.org/wiki/JavaScript',
+  'https://en.wikipedia.org/wiki/Node.js',
+  'https://en.wikipedia.org/wiki/Google_Chrome'
+];
