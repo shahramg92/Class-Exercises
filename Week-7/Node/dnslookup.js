49dc94addf2a347b25b9ca46b8722c2dd9a59f98
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-31 12:38:03 -0500

    update

---
+++ Week-7/Node/dnslookup.js
@@ -0,0 +1,8 @@
+// Write a program that prompts the user for a domain name, looks up the IP address for the domain, and prints it out. Example:
+//
+// $ node dns_lookup.
+// Domain name: yahoo.com
+// IP Address: 98.139.183.24
+// Trigger an error condition by providing an invalid domain. See that the error gets displayed.
+//
+// Hint: Use require('dns') and dns.lookup.
