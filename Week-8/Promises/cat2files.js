5ade984d75166f0724e8819ac069ec632fc57feb
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-31 16:41:17 -0500

    combine content

---
+++ Week-8/Promises/cat2files.js
@@ -0,0 +1,3 @@
+// Cat 2 Files
+//
+// Write a function that takes two input filenames and one output filename. Read the files and combine the file contents. Write the combined contents to the output file. Use a promise style to chain the reading and writing together.
