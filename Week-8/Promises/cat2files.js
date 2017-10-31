cf896d1f1531ef6d6f608a2312b6b21863ae10f4
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-31 16:57:20 -0500

    combine content

--- Week-8/Promises/cat2files.js
+++ Week-8/Promises/cat2files.js
@@ -1,3 +1,14 @@
 // Cat 2 Files
 //
 // Write a function that takes two input filenames and one output filename. Read the files and combine the file contents. Write the combined contents to the output file. Use a promise style to chain the reading and writing together.
+
+var readline = require('readline');
+var fs = require('fs');
+
+
+var r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdin
+});
+
+r1.question("Enter a filename you want to use as input")
