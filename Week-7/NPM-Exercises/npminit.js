f219cf4152e4bf67d360e1139383a59443609c93
Shahram Ghassemi <shahramg92@hotmail.com>
2017-10-31 13:06:07 -0500

    update

---
+++ Week-7/NPM-Exercises/npminit.js
@@ -0,0 +1,3 @@
+// Create a directory for today's work. Run npm init within it to create a package.json file within it. For the rest of these exercises, you will install modules using the --save option, so that the dependency will be remembered in your package.json file. After each time you install a module, check your package.json and see that what you installed has been added as a dependency.
+//
+// Create a .gitignore file, and within it, put node_modules, so that git will ignore the node_modules folder when you are adding files to be tracked.
