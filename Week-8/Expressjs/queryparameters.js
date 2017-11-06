0df9a0e336c52c21096ae638f11924e8e7d0c1cf
Shahram Ghassemi <shahramg92@hotmail.com>
2017-11-06 10:55:34 -0600

    expressjs

---
+++ Week-8/Expressjs/queryparameters.js
@@ -0,0 +1,30 @@
+// Query Parameters: Tell the year you were born
+//
+// Adding to the same program, display the year you were born when you report your age in a query parameter. For example, when you go to the URL:
+//
+// /year?age=32 it will display You were born in 1985.
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (request, response) {
+  response.send('Hello World!');
+});
+app.get('/cats', function (request, response) {
+  response.send('Meow')
+})
+app.get('/dogs', function (request, response) {
+  response.send('Woof')
+})
+app.get('/cats_and_dogs', function (request, response) {
+  response.send('Living together')
+})
+app.get('/hello/:name', function(request, response) {
+  var name = request.params.name || 'world';
+  response.send('Hello ' + name + '!');
+});
+
+
+app.listen(8000, function () {
+  console.log('Listening on port 8000');
+});
