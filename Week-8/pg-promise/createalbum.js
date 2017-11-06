39193b3161df4197fdd76d5e0da0ac35592866aa
Shahram Ghassemi <shahramg92@hotmail.com>
2017-11-06 11:02:18 -0600

    pg-promises

---
+++ Week-8/pg-promise/createalbum.js
@@ -0,0 +1,11 @@
+// Create album
+//
+// Write a script to create a new album in the database. You may either connect to your local database or your remote database. The script will prompt the user for an album name, a year, and an artist ID. You may use the prompt-promise module to get user prompts in the promise style - with this everything can be written in one straight chain. Be sure to sanitize your inputs!
+//
+// Example session:
+//
+// $ node create_album.js
+// Album name? The Squeezed Apple
+// Album year? 1998
+// Artist ID? 4
+// Created album with ID 2.
---
+++ Week-8/pg-promise/createartist.js
@@ -0,0 +1,9 @@
+// Create Track
+//
+// Write a script to create a track.
+//
+// $ node create_track.js
+// Track name? I like crunchy soft
+// Album ID? 2
+// Track Duration? 5:33
+// Create track with ID 13.
